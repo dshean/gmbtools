e6c904511abe5d32e2327d16998dd7d7761a03fd
David Shean <dshean@gmail.com>
2017-05-24 15:04:19 -0700

    lidar_proc: add output file and mosaic generation notes

--- conus/lidar_proc.sh
+++ conus/lidar_proc.sh
@@ -3,6 +3,8 @@
 #Many of the CONUS lidar datasets have horizontal and vertical units of US feet
 #See doc/lidar_notes_20161024.txt for specific examples
 
+gdal_opt="-co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER"
+
 in=$1
 #Convert vertical units to meters
 image_calc -o ${in%.*}_unitmeters.tif -c "var_0*0.3048" -d float32 --threads 7 $in 
@@ -21,4 +23,32 @@ dem_geoid --reverse-adjustment ${in%.*}_unitmeters.tif
 #This tif should be fine for comparison with other datasets, could also reproject to something more desirable
 echo ${in%.*}_unitmeters-adj.tif
 
+i=${in%.*}_unitmeters-adj
 
+if [ ! -e ${i}_32610_1m.tif ] ; then
+    gdalwarp -tr 1 1 $gdal_opt -t_srs EPSG:32610 -r cubic -dstnodata -9999 $i ${i}_32610_1m.tif
+fi
+
+if [ ! -e ${i}_32610_1m.tif.ovr ] ; then 
+    ~/src/demtools/gdaladdo_ro.sh ${i}_32610_1m.tif
+fi
+
+if [ ! -e ${i}_32610_32m.tif ] ; then 
+    gdalwarp -overwrite -tr 32 32 -r cubic $gdal_opt ${i}_32610_1m.tif ${i}_32610_32m.tif
+fi
+
+if [ ! -e ${i}_32610_32m_hs_az315.tif ] ; then 
+    hs.sh ${i}_32610_32m.tif
+fi
+
+#ned=/Volumes/d/ned/NED_2003_1arcsec/ned1_2003_adj.vrt
+#if [ ! -e ned_diff/$(basename ${ned%.*})_$(basename ${i}_32610_32m_dz_eul.tif) ] ; then 
+#    compute_dz.py -outdir ned_diff $ned ${i}_32610_32m.tif
+#fi
+
+#Now build supermosaic
+#list=$(find . -name '*32610_32m.tif')
+#gdalbuildvrt conus_lidar_32m.vrt $list
+#hs.sh conus_lidar_32m.vrt
+#gdaladdo_ro.sh conus_lidar_32m.vrt
+#gdaladdo_ro.sh conus_lidar_32m_hs_az315.tif
