c9c05229556fe8b12870a0f0c103294e8e5b1161
David Shean <dshean@gmail.com>
2017-06-30 11:05:30 -0700

    hma_mos: fix bugs, better filename, simplify shp

--- conus/hma_mos.sh
+++ conus/hma_mos.sh
@@ -1,6 +1,7 @@
 #! /bin/bash 
 
 #qsub -I -q devel -lselect=1:model=bro,walltime=2:00:00
+#cd /nobackup/deshean/hma
 
 count=true
 index=true
@@ -13,12 +14,13 @@ mos=~/src/Tools/dem_mosaic_validtiles.py
 
 ts=`date +%Y%m%d`
 
-out=hma_${res}m_mos_${ts}/hma_${res}m
-out=hma_${res}m_mos_20170601/hma_${res}m
+out=mos/hma_${ts}_mos/mos_${res}m/hma_${ts}_mos_${res}m
+#out=hma_${res}m_mos_20170601/hma_${res}m
 
 #hma
 proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
 
+#Should add option to split annually
 echo "Identifying input DEMs"
 list=$(ls */*/*00/dem*/*-DEM_${res}m.tif */*00/dem*/*-DEM_${res}m.tif)
 #list=$(ls */*/*/dem*/*-DEM_8m_trans.tif | grep -v QB)
@@ -47,14 +49,14 @@ if (( "$res" == "32" )) ; then
         gdalwarp -tr $lowres $lowres $gdal_opt $out.vrt ${out}_${lowres}m.tif
         gdaladdo_ro.sh ${out}_${lowres}m.tif
         hs.sh ${out}_${lowres}m.tif
-        gdaladdo_ro.sh ${out}_${lowres}m_az315.tif
+        gdaladdo_ro.sh ${out}_${lowres}m_hs_az315.tif
     fi
     if $count ; then
         if [ ! -e ${out}_count.vrt ] ; then
             echo "Preparing countmap"
             $mos --stat count --threads $threads --tr $res --t_srs "$proj" --georef_tile_size=$tilesize -o $out $list
         fi
-        if [ ! -e ${out}_count.vrt ] ; then
+        if [ ! -e ${out}_count_${lowres}m.tif ] ; then
             echo "Preparing lowres $lowres m countmap"
             gdalwarp -tr $lowres $lowres $gdal_opt ${out}_count.vrt ${out}_count_${lowres}m.tif
             gdaladdo_ro.sh ${out}_count_${lowres}m.tif
@@ -62,13 +64,14 @@ if (( "$res" == "32" )) ; then
     fi
     if $index ; then
         parallel -j $threads "if [ ! -e {.}_${lowres}m.tif ] ; then gdalwarp -overwrite -r cubic -t_srs \"$proj\" -tr $lowres $lowres -dstnodata -9999 {} {.}_${lowres}m.tif; fi" ::: $list
-        #Need to fix raster2shp, doesn't like subdir in merge_fn
-        #raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed 's/DEM_32m.tif/DEM_32m_100m.tif/g') 
-        raster2shp.py $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_100m.tif/g")
+        raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.tif/g") 
         for i in $list
         do
-            echo rm $(echo $i | sed "s/DEM_${res}m.tif/DEM_${res}m_100m.{shp,shx,dbf,prj}/")
+            rm_list=$(echo $i | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.{shp,shx,dbf,prj,tif}/")
+            rm -v $(eval ls $rm_list)
         done
-        ogr2ogr -f KML merge.kml merge.shp
+        tol=0.001
+        ogr2ogr -simplify $tol ${out}_stripindex_simp.shp ${out}_stripindex.shp
+        ogr2ogr -f KML ${out}_stripindex_simp.kml ${out}_stripindex_simp.shp
     fi
 fi
