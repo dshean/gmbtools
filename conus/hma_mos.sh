cc51183dac0052774aa2e6311615ed7e47dfb66f
David Shean <dshean@gmail.com>
2017-06-30 17:02:36 -0700

    hma_mos: add tileindex

--- conus/hma_mos.sh
+++ conus/hma_mos.sh
@@ -2,20 +2,22 @@
 
 #qsub -I -q devel -lselect=1:model=bro,walltime=2:00:00
 #cd /nobackup/deshean/hma
+#~/src/conus/conus/hma_mos.sh
 
 count=true
 index=true
-res=32
+tileindex=true
+res=8
 ncpu=$(cat /proc/cpuinfo | egrep "core id|physical id" | tr -d "\n" | sed s/physical/\\nphysical/g | grep -v ^$ | sort | uniq | wc -l)
 threads=$((ncpu-1))
-#threads=28
 tilesize=100000
 mos=~/src/Tools/dem_mosaic_validtiles.py
+#Simplify tolerance in decimal degrees
+tol=0.001
 
 ts=`date +%Y%m%d`
 
 out=mos/hma_${ts}_mos/mos_${res}m/hma_${ts}_mos_${res}m
-#out=hma_${res}m_mos_20170601/hma_${res}m
 
 #hma
 proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
@@ -63,15 +65,24 @@ if (( "$res" == "32" )) ; then
         fi
     fi
     if $index ; then
-        parallel -j $threads "if [ ! -e {.}_${lowres}m.tif ] ; then gdalwarp -overwrite -r cubic -t_srs \"$proj\" -tr $lowres $lowres -dstnodata -9999 {} {.}_${lowres}m.tif; fi" ::: $list
-        raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.tif/g") 
-        for i in $list
-        do
-            rm_list=$(echo $i | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.{shp,shx,dbf,prj,tif}/")
-            rm -v $(eval ls $rm_list)
-        done
-        tol=0.001
-        ogr2ogr -simplify $tol ${out}_stripindex_simp.shp ${out}_stripindex.shp
-        ogr2ogr -f KML ${out}_stripindex_simp.kml ${out}_stripindex_simp.shp
+        if [ ! -e ${out}_stripindex.shp ] ; then
+            parallel -j $threads "if [ ! -e {.}_${lowres}m.tif ] ; then gdalwarp -overwrite -r cubic -t_srs \"$proj\" -tr $lowres $lowres -dstnodata -9999 {} {.}_${lowres}m.tif; fi" ::: $list
+            raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.tif/g") 
+            echo "Removing intermediate files"
+            for i in $list
+            do
+                rm_list=$(echo $i | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.{shp,shx,dbf,prj,tif}/")
+                rm $(eval ls $rm_list)
+            done
+            ogr2ogr -simplify $tol ${out}_stripindex_simp.shp ${out}_stripindex.shp
+            if [ ! -e ${out}_stripindex_simp.kml ] ; then 
+                ogr2ogr -f KML ${out}_stripindex_simp.kml ${out}_stripindex_simp.shp
+            fi
+        fi
+    fi
+    #This is a hack for now, should add this functionality to dem_mosaic_validtiles.py
+    if $tileindex ; then 
+        raster2shp.py -merge_fn ${out}_tileindex.shp $(gdalinfo $out.vrt | grep tif)
+        ogr2ogr -simplify $tol ${out}_tileindex_simp.shp ${out}_tileindex.shp
     fi
 fi
