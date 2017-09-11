a5bdf53330de0b70d38ff99abb603a90feadae77
David Shean <dshean@gmail.com>
2017-09-10 22:04:26 -0700

    site_rerun_demprep: renamed, update

--- gmbtools/site_rerun_demprep.sh
+++ gmbtools/site_rerun_demprep.sh
@@ -7,14 +7,18 @@ dem=$1
 ref_src=/nobackup/deshean/rpcdem/ned13/ned13_tiles_glac24k_115kmbuff.vrt 
 
 #Prepare reference DEM
-warptool.py -outdir . -te $dem -tr $dem -t_srs $dem $ref_src
-ref=$(basename $ref_src)
-ref=${ref%.*}_warp.tif
+ref_bn=$(basename $ref_src)
+if [ ! -e ${ref_bn%.*}_warp.tif ] ; then 
+    warptool.py -outdir . -te $dem -tr $dem -t_srs $dem $ref_src
+fi
+ref=${ref_bn%.*}_warp.tif
 
 #Remove outliers based elev diff
 #Note, NED is a DSM, so will be ~20-50 m offsets for forested areas
-filter.py $dem -filt dz -param $ref 0 100 
-dem=${dem%.*}_dzfilt*.tif
+if [ ! -e ${dem%.*}_dzfilt_0.00-100.00.tif ] ; then 
+    filter.py $dem -filt dz -param $ref 0 100 
+fi
+dem=${dem%.*}_dzfilt_0.00-100.00.tif
 #filter.py $dem -filt med -param 5 
 #dem=${dem%.*}_medfilt_5px.tif
 
@@ -22,7 +26,9 @@ dem=${dem%.*}_dzfilt*.tif
 gauss_fn=''
 for s in 5 11 21
 do
-    filter.py $dem -filt gauss -param $s 
+    if [ ! -e ${dem%.*}_gaussfilt_${s}px.tif ] ; then 
+        filter.py $dem -filt gauss -param $s 
+    fi
     gauss_fn+=" ${dem%.*}_gaussfilt_${s}px.tif"
 done
 
@@ -39,3 +45,5 @@ done
 
 #Merge gauss pyramid
 dem_mosaic --priority-blending-length 5 -o ${dem%.*}_gaussfill $dem $gauss_fn $ref
+
+echo ${dem%.*}_gaussfill-tile-0.tif
