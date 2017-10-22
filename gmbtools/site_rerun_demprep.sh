2874292f8722335e833f2d5128c03cead685f28b
David Shean <dshean@gmail.com>
2017-10-21 22:15:33 -0700

    site_rerun scripts update, wip

--- gmbtools/site_rerun_demprep.sh
+++ gmbtools/site_rerun_demprep.sh
@@ -2,27 +2,30 @@
 
 #This script can be used to prepare a new basemap for a second iteration of high-quality DEM generation
 
-#This assumes output from conus_site_poly.py
+#This assumes output from site_query.py
 #dem=rainier_stack_all-tile-0.tif
 
 dem=$1
 #ref_src=/nobackup/deshean/rpcdem/ned13/ned13_tiles_glac24k_115kmbuff.vrt 
-ref_src=/nobackup/deshean/hma/mos/latest/mos_8m/*mos_8m.vrt
+ref_src=/nobackup/deshean/rpcdem/hma/srtm1/hma_srtm_gl1.vrt
+#ref_src=/nobackup/deshean/hma/mos/latest/mos_8m/*mos_8m.vrt
 #ref_src=/nobackup/deshean/hma/mos/latest/mos_32m/*mos_32m.vrt
+
+max_dz=200
 
 #Prepare reference DEM
 ref_bn=$(basename $ref_src)
-if [ ! -e ${ref_bn%.*}_warp.tif ] ; then 
-    warptool.py -outdir . -te $dem -tr $dem -t_srs $dem $ref_src
+ref=$(dirname $dem)/${ref_bn%.*}_warp.tif
+if [ ! -e $ref ] ; then 
+    warptool.py -outdir $(dirname $dem) -te $dem -tr $dem -t_srs $dem $ref_src
 fi
-ref=${ref_bn%.*}_warp.tif
 
 #Remove outliers based elev diff
 #Note, NED is a DSM, so will be ~20-50 m offsets for forested areas
-if [ ! -e ${dem%.*}_dzfilt_0.00-100.00.tif ] ; then 
-    filter.py $dem -filt dz -param $ref 0 100 
+if [ ! -e ${dem%.*}_dzfilt_0.00-${max_dz}.00.tif ] ; then 
+    filter.py $dem -filt dz -param $ref 0 $max_dz 
 fi
-dem=${dem%.*}_dzfilt_0.00-100.00.tif
+dem=${dem%.*}_dzfilt_0.00-${max_dz}.00.tif
 #filter.py $dem -filt med -param 5 
 #dem=${dem%.*}_medfilt_5px.tif
 
@@ -51,3 +54,5 @@ done
 dem_mosaic --priority-blending-length 5 -o ${dem%.*}_gaussfill $dem $gauss_fn $ref
 
 echo ${dem%.*}_gaussfill-tile-0.tif
+
+#rm $gauss_fn $ref
--- gmbtools/site_rerun_wrapper.sh
+++ gmbtools/site_rerun_wrapper.sh
@@ -33,42 +33,54 @@ topdir=/nobackupp8/deshean/hma
 cd $topdir
 #make_mos.sh 
 #Get latest strip index
-strip_shp=$(ls -tr mos/latest/mos_32m/*mos_32m_stripindex_simp.shp | tail -1)
+#strip_shp=$(ls -tr mos/latest/mos_32m/*mos_32m_stripindex_simp.shp | tail -1)
+strip_shp=$(ls -tr mos/*_mos/mos_32m/*mos_32m_stripindex_simp.shp | tail -1)
 
 #Polygons to rerun
 #site_shp=shp/rainier_rerun_wgs84.shp
-site_shp=sites/khumbu/khumbu_clip.shp
-#conus_site_poly.sh $site_shp $strip_shp
+#site_shp=sites/khumbu/khumbu_clip.shp
+site_shp=shp/hma_sites_20171004.shp
 
-ogr2ogr -clipsrc $site_shp ${site_shp%.*}_stripindex.shp $strip_shp
-ogrinfo -al ${site_shp%.*}_stripindex.shp | grep name | awk '{print $NF}' | awk -F'/' '{print $3}' > ${site_shp%.*}_stripindex_pairlist.txt
+#This was a quick workaround to site_query.py
+#ogr2ogr -clipsrc $site_shp ${site_shp%.*}_stripindex.shp $strip_shp
+#ogrinfo -al ${site_shp%.*}_stripindex.shp | grep name | awk '{print $NF}' | awk -F'/' '{print $3}' > ${site_shp%.*}_stripindex_pairlist.txt
 
-cd sites
+#This will create lists of overlapping files, stacks and mosaics
+#site_query.py $site_shp $strip_shp
+
+cd sites2
 for site in $(ls -d *)
 do
+    echo $site
     dem=$(ls $site/stack_all/${site}_stack_all-tile-0.tif)
     #Might need to update site_rerun_demproc.sh to handle relative paths
-    dem_rerun=$site/stack_all/${site}_stack_all-tile-0_dzfilt_0.00-100.00_gaussfill-tile-0.tif
-    if [ ! -e $dem_rerun ] ; then 
-        dem_rerun=$(site_rerun_demproc.sh $dem | tail -1)
+    dem_rerun=$(ls $site/stack_all/${site}_stack_all-tile-0_dzfilt_0.00-*.00_gaussfill-tile-0.tif 1> /dev/null 2>&1)
+    #if [ ! -e $dem_rerun ] ; then 
+    if $dem_rerun ; then 
+        echo "Running site_rerun_demprep.sh to create filled DEM for rerun"
+        dem_rerun=$(site_rerun_demprep.sh $dem | tail -1)
     fi
     dem_fnlist=$(cat $site/site_${site}.csv | grep tif)
-    dem_pairlist=$(cat $site/site_${site}.csv | grep tif | awk -F'/' '{print $5}')
+    #dem_pairlist=$(cat $site/site_${site}.csv | grep tif | awk -F'/' '{print $5}')
+    dem_pairlist=$(cat $site/site_${site}.csv | grep tif | awk -F'/' '{print $5 "/" $6}')
 
-    if [ ! -d rerun ] ; then
-        mkdir rerun
+    if [ ! -d $site/rerun ] ; then
+        mkdir $site/rerun
     fi
-    cd rerun
+    pushd $site/rerun
     for pair in $dem_pairlist
     do
-        mkdir $pair
+        mkdir -p $pair
+        #HMA
         #r100_fn_list=$(ls $topdir/$pair/*r100.{tif,xml})
-        r100_fn_list=$(ls ../../../$pair/1*r100.{tif,xml})
-        for i in $r100_fn_list
-        do
-            ln -s ../$i $pair/ 
-        done
+        #CONUS
+        #r100_fn_list=$(ls ../../../$pair/1*r100.{tif,xml})
+        #for i in $r100_fn_list
+        #do
+        #    ln -s ../$i $pair/ 
+        #done
     done
+    pushd 
 
     #Currently, need to edit new_dg_stereo.sh
     #Update rpcdem
@@ -84,4 +96,5 @@ do
 
     #Generate new orthoimages
     #parallel --jobs 14 --delay 1 --verbose --progress 'ortho_proc.sh {}' ::: *00
+    echo
 done
