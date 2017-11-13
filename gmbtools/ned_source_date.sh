b5d0207517e806e652f104dad8a0962e9479cf6a
David Shean <dshean@gmail.com>
2017-11-12 18:15:15 -0800

    ned_source_date: update, wip

--- gmbtools/ned_source_date.sh
+++ gmbtools/ned_source_date.sh
@@ -4,7 +4,32 @@
 
 #Downloaded all tiles
 #Processed
-cd /scr2/ned/NED_2003_1arcsec
+cd /Volumes/d/ned/NED_2003_1arcsec
+
+#Create mosaic in original WGS84 coord
+fn_list=$(ls -d dem*/ | sed 's#/##g')
+gdalbuildvrt ned1_2003_wgs84.vrt $fn_list
+gdaladdo_ro.sh ned1_2003_wgs84.vrt
+
+shp_fn=meta0306_PAL_24k_10kmbuffer_clean.shp
+#Split using QGSI tool Vector -> Data Mangement Tools -> Split Vector Layer
+#Split based on META0306 (seems to be tile ID)
+d_list=$(ogrinfo -al $shp_fn | grep META0306 | awk '{print $NF}' | sed '1,2d' | sort -u)
+#for d in $d_list ; do ogr2ogr -where "META0306=$d" meta0306_split/${d}_s_date.shp $shp_fn ; done
+cd meta0306_split
+shp_list=$(ls *.shp)
+for i in $shp_list
+do
+    date=$(ogrinfo -al $i | grep S_DATE_CLN | awk '{print $NF}' | sed '1d')
+    shp_rename.sh $i ${i%.*}_S_DATE_CLN_${date}.shp
+done
+shp_list=$(ls *.shp)
+parallel 'clip_raster_by_shp.py -out_fn {.}.tif -extent shp ../ned1_2003_wgs84.vrt {}' ::: *shp  
+
+exit
+
+#Previous processing
+
 epsg=26911
 fn_list=$(ls -d dem*/)
 parallel "gdalwarp -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER -overwrite -r cubic -t_srs EPSG:$epsg -dstnodata -9999 -tr 30 30 {} {.}_${epsg}.tif; dem_geoid --threads 1 --reverse-adjustment {.}_${epsg}.tif" ::: $fn_list
