497b8c758b2b5cd7a51611570e7aee8907fffeb0
David Shean <dshean@gmail.com>
2018-08-29 20:09:57 -0700

    dem_mosaic_validtiles: Allow list of stats to process, improved parallelization

--- gmbtools/dem_mosaic_validtiles.py
+++ gmbtools/dem_mosaic_validtiles.py
@@ -37,14 +37,15 @@ from dem_mosaic_index_ts import make_dem_mosaic_index_ts
 #resource.setrlimit(resource.RLIMIT_NOFILE,(resource.RLIM_INFINITY, resource.RLIM_INFINITY))
 
 def getparser():
-    stat_choices = ['first', 'firstindex', 'last', 'lastindex', 'min', 'max', 'stddev', 'count', 'median']
+    stat_choices = ['first', 'firstindex', 'last', 'lastindex', 'min', 'max', 'mean', 'stddev', 'count', 'median', 'nmad', 'wmean']
     parser = argparse.ArgumentParser(description='Wrapper for dem_mosaic that will only write valid tiles')
     parser.add_argument('--tr', default='min', help='Output resolution (default: %(default)s)')
     parser.add_argument('--t_projwin', default='union', help='Output extent (default: %(default)s)')
     parser.add_argument('--t_srs', default='first', help='Output projection (default: %(default)s)')
     parser.add_argument('--georef_tile_size', type=float, default=100000., help='Output tile width (meters)')
-    parser.add_argument('--threads', type=int, default=iolib.cpu_count(), help='Number of simultaneous jobs to run')
-    parser.add_argument('--stat', type=str, default=None, choices=stat_choices, help='Statistic to use (default: weighted mean)')
+    parser.add_argument('--threads', type=int, default=iolib.cpu_count(logical=False), help='Number of simultaneous dem_mosaic processes to run')
+    parser.add_argument('--stat', type=str, nargs='*', default=None, choices=stat_choices, \
+            help='Specify space-delimited list of output statistics to pass to dem_mosaic (e..g, "count stddev", default: wmean)')
     parser.add_argument('-o', type=str, default=None, help='Output mosaic prefix')
     parser.add_argument('src_fn_list', type=str, nargs='+', help='Input filenames (img1.tif img2.tif ...)')
     return parser
@@ -53,11 +54,15 @@ def main():
     parser = getparser()
     args = parser.parse_args()
 
-    stat = args.stat
-    if stat is not None:
-        print("Mosaic type: %s" % stat)
-    else:
-        print("Mosaic type: Weighted average")
+    stat_list = ['wmean',]
+    if args.stat is not None:
+        if isinstance(args.stat, str):
+            stat_list = args.stat.split()
+        else:
+            stat_list = args.stat
+
+    print("The following mosaics will be generated:")
+    print(stat_list)
 
     #Tile dimensions in output projected units (meters)
     #Assume square
@@ -193,70 +198,74 @@ def main():
     outf = open(os.devnull, 'w') 
     #outf = open('%s-log-dem_mosaic-tile-%i.log' % (o, tile), 'w')
 
-    tile_fn_list = []
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        print("Running dem_mosaic in parallel with %i threads" % threads)
-        for n, tile in enumerate(out_tile_list):
-            #print('%i of %i tiles: %i' % (n+1, len(out_tile_list), tile))
-            tile_fn = '%s-tile-%0*i.tif' % (o, ni, tile)
-            if stat is not None:
-                tile_fn = os.path.splitext(tile_fn)[0]+'-%s.tif' % stat
-            dem_mosaic_args = {'fn_list':tile_dict[tile]['fn_list'], 'o':tile_fn, 'tr':tr, 't_srs':t_srs, \
-                    't_projwin':tile_dict[tile]['extent'], 'threads':1, 'stat':stat}
-            if not os.path.exists(tile_fn):
-                #This passes only files that intersect the tile, but issues with dem_mosaic reducing bounding box
-                #dem_mosaic_args[0] = tile_dict[tile]['fn_list']
-                cmd = geolib.get_dem_mosaic_cmd(**dem_mosaic_args)
-                #print(cmd)
-                executor.submit(subprocess.call, cmd, stdout=outf, stderr=subprocess.STDOUT)
-            tile_fn_list.append(tile_fn)
-            time.sleep(delay)
+    for stat in stat_list:
+        print("\nMosaic type: %s" % stat)
 
-    #Convert dem_mosaic index files to timestamp arrays
-    if stat in ['lastindex', 'firstindex']:
-        print("Running dem_mosaic_index_ts in parallel with %i threads" % threads)
-        from multiprocessing import Pool
-        pool = Pool(processes=threads)
-        results = pool.map(make_dem_mosaic_index_ts, tuple(tile_fn_list), 1)
-        #results.wait()
-        #Update filenames
-        tile_fn_list = [os.path.splitext(tile_fn)[0]+'_ts.tif' for tile_fn in tile_fn_list]
+        tile_fn_list = []
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            print("Running dem_mosaic in parallel with %i threads" % threads)
+            for n, tile in enumerate(out_tile_list):
+                #print('%i of %i tiles: %i' % (n+1, len(out_tile_list), tile))
+                tile_fn = '%s-tile-%0*i.tif' % (o, ni, tile)
+                if stat is not None:
+                    tile_fn = os.path.splitext(tile_fn)[0]+'-%s.tif' % stat
+                dem_mosaic_args = {'fn_list':tile_dict[tile]['fn_list'], 'o':tile_fn, 'tr':tr, 't_srs':t_srs, \
+                        't_projwin':tile_dict[tile]['extent'], 'threads':1, 'stat':stat}
+                if not os.path.exists(tile_fn):
+                    #This passes only files that intersect the tile, but issues with dem_mosaic reducing bounding box
+                    #dem_mosaic_args[0] = tile_dict[tile]['fn_list']
+                    cmd = geolib.get_dem_mosaic_cmd(**dem_mosaic_args)
+                    #print(cmd)
+                    executor.submit(subprocess.call, cmd, stdout=outf, stderr=subprocess.STDOUT)
+                tile_fn_list.append(tile_fn)
+                time.sleep(delay)
+
+        #Convert dem_mosaic index files to timestamp arrays
+        if stat in ['lastindex', 'firstindex']:
+            print("Running dem_mosaic_index_ts in parallel with %i threads" % threads)
+            from multiprocessing import Pool
+            pool = Pool(processes=threads)
+            results = pool.map(make_dem_mosaic_index_ts, tile_fn_list)
+            pool.close()
+            #results.wait()
+            #Update filenames
+            tile_fn_list = [os.path.splitext(tile_fn)[0]+'_ts.tif' for tile_fn in tile_fn_list]
+
+        print("\nCreating vrt of valid tiles")
+        #tile_fn_list = glob.glob(o+'-tile-*.tif')
+        vrt_fn = o+'.vrt'
+        if stat is not None:
+            vrt_fn = os.path.splitext(vrt_fn)[0]+'_%s.vrt' % stat
+            if stat in ['lastindex', 'firstindex']:
+                vrt_fn = os.path.splitext(vrt_fn)[0]+'_ts.vrt'
+        cmd = ['gdalbuildvrt', vrt_fn] 
+        vrt_fn_list = []
+        for tile_fn in tile_fn_list:
+            if os.path.exists(tile_fn):
+                vrt_fn_list.append(tile_fn)
+            else:
+                print("Missing file: %s" % tile_fn)
+        cmd.extend(vrt_fn_list)
+        print(cmd)
+        subprocess.call(cmd)
+
+        #Should create tile index shp/kml from tile_geom
+
+        #This cleans up all of the log txt files (potentially 1000s of files)
+        #Want to preserve these, as they contain list of DEMs that went into each tile
+        log_fn_list = glob.glob(o+'*%s.tif-log-dem_mosaic-*.txt' % stat)
+        print("\nCleaning up %i dem_mosaic log files" % len(log_fn_list))
+        if stat is not None:
+            tar_fn = o+'_%s_dem_mosaic_log.tar.gz' % stat
+        else:
+            tar_fn = o+'_dem_mosaic_log.tar.gz'
+        with tarfile.open(tar_fn, "w:gz") as tar:
+            for log_fn in log_fn_list:
+                tar.add(log_fn)
+        for log_fn in log_fn_list:
+            os.remove(log_fn)
 
     outf = None
-
-    print("\nCreating vrt of valid tiles")
-    #tile_fn_list = glob.glob(o+'-tile-*.tif')
-    vrt_fn = o+'.vrt'
-    if stat is not None:
-        vrt_fn = os.path.splitext(vrt_fn)[0]+'_%s.vrt' % stat
-        if stat in ['lastindex', 'firstindex']:
-            vrt_fn = os.path.splitext(vrt_fn)[0]+'_ts.vrt'
-    cmd = ['gdalbuildvrt', vrt_fn] 
-    vrt_fn_list = []
-    for tile_fn in tile_fn_list:
-        if os.path.exists(tile_fn):
-            vrt_fn_list.append(tile_fn)
-        else:
-            print("Missing file: %s" % tile_fn)
-    cmd.extend(vrt_fn_list)
-    print(cmd)
-    subprocess.call(cmd)
-
-    #Should create tile index shp/kml from tile_geom
-
-    #This cleans up all of the log txt files (potentially 1000s of files)
-    #Want to preserve these, as they contain list of DEMs that went into each tile
-    log_fn_list = glob.glob(o+'*%s.tif-log-dem_mosaic-*.txt' % stat)
-    print("\nCleaning up %i dem_mosaic log files" % len(log_fn_list))
-    if stat is not None:
-        tar_fn = o+'_%s_dem_mosaic_log.tar.gz' % stat
-    else:
-        tar_fn = o+'_dem_mosaic_log.tar.gz'
-    with tarfile.open(tar_fn, "w:gz") as tar:
-        for log_fn in log_fn_list:
-            tar.add(log_fn)
-    for log_fn in log_fn_list:
-        os.remove(log_fn)
 
 if __name__ == "__main__":
     main()
