9c00c61a308480edff3f28117cb28d020ae6cc6c
David Shean <dshean@gmail.com>
2018-10-08 14:06:52 -0700

    dem_mosaic_validtiles: update to output text file with commands, distribute across nodes, wip

--- gmbtools/dem_mosaic_validtiles.py
+++ gmbtools/dem_mosaic_validtiles.py
@@ -2,16 +2,6 @@
 """
 Run dem_mosaic in parallel for valid tiles only
 """
-#res=32
-#res=8
-#mkdir conus_${res}m_tile
-#lfs setstripe conus_${res}m_tile --count 64
-#dem_mosaic_validtiles.py --tr $res --t_projwin 'union' --t_srs '+proj=aea +lat_1=36 +lat_2=49 +lat_0=43 +lon_0=-115 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ' --georef_tile_size 100000 -o conus_${res}m_tile/conus_${res}m *00/*/*DEM_${res}m.tif
-
-#res=8
-#mkdir hma_${res}m_tile
-#lfs setstripe hma_${res}m_tile --count 64
-#dem_mosaic_validtiles.py --tr $res --t_projwin 'union' --t_srs '+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ' --georef_tile_size 100000 -o hma_${res}m_tile/hma_${res}m */*00/*/*DEM_${res}m.tif
 
 import os
 import sys
@@ -45,8 +35,9 @@ def getparser():
     parser.add_argument('--georef_tile_size', type=float, default=100000., help='Output tile width (meters)')
     parser.add_argument('--threads', type=int, default=iolib.cpu_count(logical=False), help='Number of simultaneous dem_mosaic processes to run')
     parser.add_argument('--stat', type=str, nargs='*', default=None, choices=stat_choices, \
-            help='Specify space-delimited list of output statistics to pass to dem_mosaic (e..g, "count stddev", default: wmean)')
+            help='Specify space-delimited list of output statistics to pass to dem_mosaic (e.g., "count stddev", default: wmean)')
     parser.add_argument('-o', type=str, default=None, help='Output mosaic prefix')
+    #parser.add_argument('-i', type=str, default=None, help='Input file list (e.g., fn_list.txt)')
     parser.add_argument('src_fn_list', type=str, nargs='+', help='Input filenames (img1.tif img2.tif ...)')
     return parser
 
@@ -72,30 +63,17 @@ def main():
     #This is number of simultaneous processes, each with one thread
     threads = args.threads
 
-    o = args.o
-    if o is None:
-        o = 'mos_%im/mos' % tr
-    odir = os.path.dirname(o)
-    #If dirname is empty, use prefix for new directory
-    if not odir:
-        odir = o
-        o = os.path.join(odir, o)
-    if not os.path.exists(odir): 
-        os.makedirs(odir)
-    iolib.setstripe(odir, threads)
-
-    out_pickle_fn = o+'_tile_dict.pkl'
-    """
-    if os.path.exists(out_pickle_fn):
-        with open(out_pickle_fn, 'wb') as f:
-            tile_dict = pickle.load(f)
-    else:
-    """
-
-    #Input filelist
-    fn_list = args.src_fn_list
-    #Sort?
     #Might hit OS open file limit here
+    #Workaround is to provide input filelist
+    if len(args.src_fn_list) == 1 and os.path.splitext(args.src_fn_list[0])[-1] == '.txt':
+        print("Reading filenames from input text file")
+        with open(args.src_fn_list[0]) as f:
+            fn_list = f.read().splitlines()
+    else:
+        fn_list = args.src_fn_list
+
+    #Sort?
+
     print("Loading input datasets")
     print("Note: this could take several minutes depending on number of inputs and I/O performance")
     ds_list = []
@@ -123,78 +101,100 @@ def main():
     t_projwin = geolib.extent_round(t_projwin, tr)
     mos_xmin, mos_ymin, mos_xmax, mos_ymax = t_projwin
 
-    #Compute extent geom for all input datsets
-    print("Computing extent geom for all input datasets")
-    input_geom_dict = OrderedDict()
-    for ds in ds_list:
-        ds_geom = geolib.ds_geom(ds, t_srs)
-        ds_fn = ds.GetFileList()[0]
-        #Could use filename as key here
-        input_geom_dict[ds_fn] = geolib.geom_dup(ds_geom)
-        ds = None
+    #Create output directory
+    o = args.o
+    if o is None:
+        o = 'mos_%im/mos' % tr
+    odir = os.path.dirname(o)
+    #If dirname is empty, use prefix for new directory
+    if not odir:
+        odir = o
+        o = os.path.join(odir, o)
+    if not os.path.exists(odir): 
+        os.makedirs(odir)
+    iolib.setstripe(odir, threads)
 
-    ds_list = None
+    out_pickle_fn = o+'_tile_dict.pkl'
+    if os.path.exists(out_pickle_fn):
+        with open(out_pickle_fn, 'wb') as f:
+            tile_dict = pickle.load(f)
+    else:
+        #Compute extent geom for all input datsets
+        print("Computing extent geom for all input datasets")
+        input_geom_dict = OrderedDict()
+        for n, ds in enumerate(ds_list):
+            if (n % 100 == 0):
+                print('%i of %i done' % (n, len(ds_list)))
+            ds_geom = geolib.ds_geom(ds, t_srs)
+            ds_fn = ds.GetFileList()[0]
+            #Could use filename as key here
+            input_geom_dict[ds_fn] = geolib.geom_dup(ds_geom)
+            ds = None
 
-    #Mosaic tile size
-    #Should have float extent and tile dim here
-    ntiles_w = int(math.ceil((mos_xmax - mos_xmin)/tile_width))
-    ntiles_h = int(math.ceil((mos_ymax - mos_ymin)/tile_height))
-    ntiles = ntiles_w * ntiles_h
-    print("%i (%i cols x %i rows) tiles required for full mosaic" % (ntiles, ntiles_w, ntiles_h))
-    #Use this for zero-padding of tile number
-    ntiles_digits = len(str(ntiles))
+        ds_list = None
 
-    print("Computing extent geom for all output tiles")
-    tile_dict = OrderedDict()
-    for i in range(ntiles_w):
-        for j in range(ntiles_h):
-            tilenum = j*ntiles_w + i
-            tile_xmin = mos_xmin + i*tile_width
-            tile_xmax = mos_xmin + (i+1)*tile_width
-            tile_ymax = mos_ymax - j*tile_height
-            tile_ymin = mos_ymax - (j+1)*tile_height
-            #Corner coord needed for geom
-            x = [tile_xmin, tile_xmax, tile_xmax, tile_xmin, tile_xmin]
-            y = [tile_ymax, tile_ymax, tile_ymin, tile_ymin, tile_ymax]
-            tile_geom_wkt = 'POLYGON(({0}))'.format(', '.join(['{0} {1}'.format(*a) for a in zip(x,y)]))
-            tile_geom = ogr.CreateGeometryFromWkt(tile_geom_wkt)
-            tile_geom.AssignSpatialReference(t_srs)
-            #tile_dict[tilenum] = tile_geom
-            tile_dict[tilenum] = {}
-            tile_dict[tilenum]['geom'] = tile_geom
-            tile_dict[tilenum]['extent'] = [tile_xmin, tile_ymin, tile_xmax, tile_ymax]
+        #Mosaic tile size
+        #Should have float extent and tile dim here
+        ntiles_w = int(math.ceil((mos_xmax - mos_xmin)/tile_width))
+        ntiles_h = int(math.ceil((mos_ymax - mos_ymin)/tile_height))
+        ntiles = ntiles_w * ntiles_h
+        print("%i (%i cols x %i rows) tiles required for full mosaic" % (ntiles, ntiles_w, ntiles_h))
+        #Use this for zero-padding of tile number
+        ntiles_digits = len(str(ntiles))
 
-    print("Computing valid intersections between input dataset geom and tile geom")
-    for tilenum in sorted(tile_dict.keys()):
-        print('%i of %i' % (tilenum, len(tile_dict.keys())))
-        tile_geom = tile_dict[tilenum]['geom']
-        tile_dict_fn = []
-        for ds_fn, ds_geom in input_geom_dict.iteritems():
-            if tile_geom.Intersects(ds_geom):
-                tile_dict_fn.append(ds_fn)
-                #Write out shp for debugging
-                #geolib.geom2shp(tile_geom, 'tile_%03i.shp' % tilenum)
-        if tile_dict_fn:
-            tile_dict[tilenum]['fn_list'] = tile_dict_fn
-    
-    #Write out dictionary with list of fn for each tile
-    print("Writing out tile dictionary")
-    with open(out_pickle_fn, 'wb') as f:
-        pickle.dump(tile_dict, f)
+        print("Computing extent geom for all output tiles")
+        tile_dict = OrderedDict()
+        for i in range(ntiles_w):
+            for j in range(ntiles_h):
+                tilenum = j*ntiles_w + i
+                tile_xmin = mos_xmin + i*tile_width
+                tile_xmax = mos_xmin + (i+1)*tile_width
+                tile_ymax = mos_ymax - j*tile_height
+                tile_ymin = mos_ymax - (j+1)*tile_height
+                #Corner coord needed for geom
+                x = [tile_xmin, tile_xmax, tile_xmax, tile_xmin, tile_xmin]
+                y = [tile_ymax, tile_ymax, tile_ymin, tile_ymin, tile_ymax]
+                tile_geom_wkt = 'POLYGON(({0}))'.format(', '.join(['{0} {1}'.format(*a) for a in zip(x,y)]))
+                tile_geom = ogr.CreateGeometryFromWkt(tile_geom_wkt)
+                tile_geom.AssignSpatialReference(t_srs)
+                #tile_dict[tilenum] = tile_geom
+                tile_dict[tilenum] = {}
+                tile_dict[tilenum]['geom'] = tile_geom
+                tile_dict[tilenum]['extent'] = [tile_xmin, tile_ymin, tile_xmax, tile_ymax]
+                tile_dict[tilenum]['tr'] = tr
+                tile_dict[tilenum]['t_srs'] = t_srs 
 
-    out_tile_list = []
-    for tilenum in tile_dict.keys():
-        if 'fn_list' in tile_dict[tilenum]:
-            out_tile_list.append(tilenum)
-        else:
-            del tile_dict[tilenum]
+        print("Computing valid intersections between input dataset geom and tile geom")
+        for tilenum in sorted(tile_dict.keys()):
+            print('%i of %i' % (tilenum, len(tile_dict.keys())))
+            tile_geom = tile_dict[tilenum]['geom']
+            tile_dict_fn = []
+            for ds_fn, ds_geom in input_geom_dict.iteritems():
+                if tile_geom.Intersects(ds_geom):
+                    tile_dict_fn.append(ds_fn)
+                    #Write out shp for debugging
+                    #geolib.geom2shp(tile_geom, 'tile_%03i.shp' % tilenum)
+            if tile_dict_fn:
+                tile_dict[tilenum]['fn_list'] = tile_dict_fn
+        
+        out_tile_list = []
+        for tilenum in tile_dict.keys():
+            if 'fn_list' in tile_dict[tilenum]:
+                out_tile_list.append(tilenum)
+            else:
+                del tile_dict[tilenum]
 
-    print("%i valid output tiles" % len(out_tile_list))
-    out_tile_list.sort()
-    out_tile_list = list(set(out_tile_list))
-    ni = max([len(str(i)) for i in out_tile_list])
-    out_tile_list_str = ' '.join(map(str, out_tile_list))
-    print(out_tile_list_str)
+        print("%i valid output tiles" % len(out_tile_list))
+        out_tile_list.sort()
+        out_tile_list = list(set(out_tile_list))
+        ni = max([len(str(i)) for i in out_tile_list])
+        out_tile_list_str = ' '.join(map(str, out_tile_list))
+        print(out_tile_list_str)
+
+        #Write out dictionary with list of fn for each tile
+        print("Writing out tile dictionary")
+        with open(out_pickle_fn, 'wb') as f:
+            pickle.dump(tile_dict, f)
 
     delay = 0.001
     outf = open(os.devnull, 'w') 
@@ -202,25 +202,48 @@ def main():
 
     #Should run the tiles with the largest file count first, as they will likely take longer
     tile_dict = OrderedDict(sorted(tile_dict.items(), key=lambda item: len(item[1]['fn_list']), reverse=True))
+    #Do tiles with smallest file count first
+    #tile_dict = OrderedDict(sorted(tile_dict.items(), key=lambda item: len(item[1]['fn_list']), reverse=False))
     out_tile_list = tile_dict.keys()
 
-    #Reorganized to run all tiles for all stats in parallel
-    #So we're not waiting for one tile to finish 
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        print("Running dem_mosaic in parallel with %i threads" % threads)
+    cmd_list = []
+    out_cmd_fn = o+'_cmd.sh'
+    with open(out_cmd_fn, 'w') as f_cmd:
         for n, tile in enumerate(out_tile_list):
             #print('%i of %i tiles: %i' % (n+1, len(out_tile_list), tile))
             tile_fn_base = '%s-tile-%0*i.tif' % (o, ni, tile)
+            tile_fn_list_txt = os.path.splitext(tile_fn_base)[0]+'_fn_list.txt'
+            #Write out DEM file list for the tile
+            with open(tile_fn_list_txt, 'w') as f_fn_list:
+                f_fn_list.write('\n'.join(tile_dict[tile]['fn_list']))
             for stat in stat_list:
                 tile_fn = os.path.splitext(tile_fn_base)[0]+'-%s.tif' % stat
-                dem_mosaic_args = {'fn_list':tile_dict[tile]['fn_list'], 'o':tile_fn, 'tr':tr, 't_srs':t_srs, \
-                        't_projwin':tile_dict[tile]['extent'], 'threads':1, 'stat':stat}
+                dem_mosaic_args = {'fn_list':tile_dict[tile]['fn_list'], 'o':tile_fn, 'fn_list_txt':tile_fn_list_txt, \
+                        'tr':tr, 't_srs':t_srs, 't_projwin':tile_dict[tile]['extent'], 'threads':1, 'stat':stat}
                 if not os.path.exists(tile_fn):
-                    #This passes only files that intersect the tile, but issues with dem_mosaic reducing bounding box
-                    #dem_mosaic_args[0] = tile_dict[tile]['fn_list']
                     cmd = geolib.get_dem_mosaic_cmd(**dem_mosaic_args)
-                    #print(cmd)
-                    executor.submit(subprocess.call, cmd, stdout=outf, stderr=subprocess.STDOUT)
+                    cmd_list.append(cmd)
+                    #Write out command to file
+                    f_cmd.write('%s\n' % ' '.join(str(i) for i in cmd))
+
+    #If we're on Pleiades, split across multiple nodes
+    #Hack with GNU parallel right now
+    import socket
+    if 'nasa' in socket.getfqdn():
+        #print("lfs setstripe -c 64 %s" % o)
+        cmd = ['lfs', 'setstripe', '-c', 64, o]
+        print(''.join(str(i) for i in cmd))
+        subprocess.call(cmd)
+        cmd = ['qsub', '-v', 'cmdfn=%s' % out_cmd_fn, '~/src/pbs_scripts/dem_mosaic_parallel.pbs']
+        print(''.join(str(i) for i in cmd))
+        subprocess.call(cmd)
+        #print("qsub -v cmd_fn=%s ~/src/pbs_scripts/dem_mosaic_parallel.pbs" % out_cmd_fn)
+        #Need to wait, maybe while qstat
+    else:
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            print("Running dem_mosaic in parallel with %i threads" % threads)
+            for cmd in cmd_list:
+                executor.submit(subprocess.call, cmd, stdout=outf, stderr=subprocess.STDOUT)
             time.sleep(delay)
 
     #Now aggegate into stats
