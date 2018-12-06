01feecf19d8ac2d04aa31b0d8811f4521ba981e7
David Shean <dshean@gmail.com>
2018-12-06 02:58:02 -0800

    make_mos: update for latlon tiles, clean up lowres, wip

--- gmbtools/make_mos.sh
+++ gmbtools/make_mos.sh
@@ -3,7 +3,7 @@
 set -e
 
 #If interrupted
-#for i in *.tif; do if ! ls ${i}-log-dem_mosaic*txt 1> /dev/null 2>&1 ; then rm -v ${i}*; else if ! grep -q 'Number of valid' $(ls -t ${i}-log-dem_mosaic*txt | head -1); then rm -v ${i}* ; fi ; fi ; done
+#for i in *-tile-[0-9]*-*.tif; do if ! ls ${i}-log-dem_mosaic*txt 1> /dev/null 2>&1 ; then rm -v ${i}*; else if ! grep -q 'Number of valid' $(ls -t ${i}-log-dem_mosaic*txt | head -1); then rm -v ${i}* ; fi ; fi ; done
 
 #pbs_rfe --duration 0+6 --model bro
 #qsub -I -q devel -lselect=1:model=bro,walltime=2:00:00
@@ -27,27 +27,28 @@ summer=false
 trans=false
 
 #Output mosaic res
-res=2
-#res=8
+#res=2
+res=8
 #res=32
 #ASTER
 #res=30
 
 #If res is better than 32, make lowres products for faster browsing
-lowres=100
+#lowres=100
+lowres=90
 #lowres=32
 
 #Write out tif mosaics at full res
 fullres_tif=false
-lowres_tif=false
+lowres_tif=true
 
 #Specify the output types
 statlist=""
-statlist+="wmean"
-#statlist+=" count"
+statlist+=" wmean"
+statlist+=" count"
 statlist+=" stddev"
-#statlist+=" median medianindex"
-#statlist+=" nmad"
+statlist+=" median medianindex"
+statlist+=" nmad"
 #statlist+=" last lastindex first firstindex"
 
 #Generate strip index shp
@@ -57,19 +58,27 @@ stripindex=false
 tileindex=false
 
 #Exclude Quickbird-2
-noQB=false
+noQB=true
 
 #Exclude GeoEye-1
 noGE=false
+GEonly=false
 
 #Default computes union from input DEMs
 extent='union'
 
 #Hardcoded sitename, projection and extent (performance improvement)
 site=hma
-proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
+#proj='+proj=aea +lat_1=25 +lat_2=47 +lat_0=36 +lon_0=85 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs '
+proj='EPSG:4326'
+extent='64 24 109 45'
+#1 arcsec
+res=0.002777777777778
+#1/3 arcsec
+res=0.000925925925926
+lowres=$(python -c "print($res*3.*3.)")
 #WV/GE extent
-extent='-1553632.99074 -1030104.4196 1727255.00926 1268847.5804'
+#extent='-1553632.99074 -1030104.4196 1727255.00926 1268847.5804'
 #ASTER extent
 #extent='-1604981.73315 -1094260.0 1847978.26685 1161996.0'
 #Should take union of these, so we have uniform tile boundaries
@@ -80,8 +89,9 @@ extent='-1553632.99074 -1030104.4196 1727255.00926 1268847.5804'
 #proj='EPSG:32615'
 
 #Mosaic tile size in meters
-tilesize=10000
+#tilesize=10000
 #tilesize=100000
+tilesize=1
 mos=~/src/gmbtools/gmbtools/dem_mosaic_validtiles.py
 #Simplify tolerance in decimal degrees
 tol=0.001
@@ -96,14 +106,15 @@ threads=$((ncpu-1))
 ts=`date +%Y%m%d`
 #ts=20181003
 #ts=20180830
+#ts=20181127_GEonly
+#ts=20181130_aster
+ts=20181205
 
 #Should add option to split annually
 echo "Identifying input DEMs"
 
-ext="-DEM_${res}m"
-
-mos_ext="${site}_mos_${res}m"
-#mos_ext=${site}_${ts}_mos
+#mos_ext="${site}_mos_${res}m"
+mos_ext="${site}_mos_13"
 
 if $latest ; then
     #Note: CONUS is only current through June 2016, missing 
@@ -127,35 +138,25 @@ if $summer ; then
     mos_ext+='_summer'
 fi
 
-if $trans ; then
-    ext+='_trans'
-    mos_ext+='_trans'
-fi
-
-#list=$(ls */*/*00/dem*/*-DEM_${res}m.tif */*00/dem*/*-DEM_${res}m.tif)
-#list=$(ls */stereo/*00/dem*/*-DEM_${res}m.tif */*00/dem*/*-DEM_${res}m.tif)
-#list=$(ls */*/*/dem*/*-DEM_8m_trans.tif | grep -v QB)
-
+#Standard WV
 #ext="-DEM_${res}m"
-#ext="-DEM_${res}m_dem_align"
-ext="-DEM_${res}m_dzfilt_-200_200"
+#list=$(ls *00/dem*/${re}*${ext}.tif)
+
+#HMA alongtrack/crosstrack
+#list=$(ls *track/*00/dem*/${re}*${ext}.tif)
+
 #WV/GE dem_align
+ext="-DEM_8m_dzfilt_-200_200_*align"
 #ext="-DEM_${res}m_dzfilt_-200_200_*align"
+#Reference products (masked to remove outliers)
+#ext="-DEM_${res}m_dzfilt_-200_200_*align_filt.tif"
+list=$(ls *track/2*align/${re}*${ext}.tif)
+
 #ASTER
 #ext="_align_dzfilt_-100_100"
+#ext='_align'
+#list=$(ls 2*/AST*_dem_align/*${re}*${ext}.tif)
 
-echo $re
-echo $ext
-echo $mos_ext
-
-#list=$(ls *00/dem*/${re}*${ext}.tif)
-#HMA alongtrack/crosstrack
-list=$(ls *track/*00/dem*/${re}*${ext}.tif)
-#WV/GE dem_align
-#list=$(ls *align/${re}*${ext}.tif)
-
-#ASTER
-#list=$(ls 2*/*cr_dem_align/*${re}*${ext}.tif)
 echo $list | wc -w
 
 if $noQB ; then
@@ -168,8 +169,13 @@ fi
 #GE1 can have nasty artifacts, should isolate and then merge GE01 composite with WV composite
 if $noGE ; then
     echo "Removing GE01"
-    #list=$(echo $list | tr ' ' '\n' | grep -v QB02)
-    list=$(echo $list | tr ' ' '\n' | grep -v '_105[0-9A-Z]*_101')
+    list=$(echo $list | tr ' ' '\n' | grep -v '_105[0-9A-Z]*_105')
+    echo $list | wc -w
+fi
+
+if $GEonly ; then
+    echo "Isolating GE01"
+    list=$(echo $list | tr ' ' '\n' | grep '_105[0-9A-Z]*_105')
     echo $list | wc -w
 fi
 
@@ -178,12 +184,12 @@ out=mos/${site}_${ts}_mos/${mos_ext}/${mos_ext}
 #Sort by date
 #NOTE: Need to update sort key with increased path depths
 #*align/*DEM.tif 
-#list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 2)
+list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 2)
 #*00/dem*/*DEM.tif
 #list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 3)
 #*track/*00/dem*/*DEM.tif
 #*00/dem*/*align/*DEM.tif
-list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 4)
+#list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 4)
 #list=$(echo $list | tr ' ' '\n' | sort -n -t'/' -k 5)
 
 echo
@@ -203,7 +209,8 @@ echo
 
 if [ ! -d $(dirname $out) ] ; then 
     mkdir -p $(dirname $out)
-    lfs setstripe -c $ncpu $(dirname $out)
+    stripecount=8
+    lfs setstripe -c $stripecount $(dirname $out)
 fi
 
 echo $list | tr ' ' '\n' > ${out}_input_DEM_list.txt
@@ -236,6 +243,15 @@ if [[ ! -z "${statlist_todo// }" ]] ; then
     eval $mos --stat $statlist_todo $mos_arg
 fi
 
+#Mask output median or wmean to preserve pixels with count > 2, nmad < 3.0
+statlist_tomask="median wmean"
+for stat in $statlist_tomask
+do
+    #parallel 'dem_mosaic_mask.py {}' ::: ${out}*-${stat}.tif
+    gdalbuildvrt -r cubic ${out}_${stat}_masked.vrt ${out}*-${stat}_masked.tif
+    statlist+=" ${stat}_masked"
+done
+
 #Define function to convert vrt to output tif products
 stat_vrt2tif() {
     in=${out}_${1}
@@ -250,21 +266,28 @@ stat_vrt2tif() {
         fi
     fi
     if $lowres_tif ; then
-        if [ ! -e ${in}_${lowres}m.tif ] ; then
+        if [ ! -e ${in}_${lowres_str}.tif ] ; then
             echo "Converting vrt to lowres tif: $in.vrt"
-            gdalwarp -tr $lowres $lowres $gdal_opt $in.vrt ${in}_${lowres}m.tif
-            echo "Adding overviews: ${in}_${lowres}m.tif"
-            gdaladdo_ro.sh ${in}_${lowres}m.tif
-            #hs.sh ${in}_${lowres}m.tif
-            #gdaladdo_ro.sh ${in}_${lowres}m_hs_az315.tif
+            gdalwarp -tr $lowres $lowres $gdal_opt $in.vrt ${in}_${lowres_str}.tif
+            echo "Adding overviews: ${in}_${lowres_str}.tif"
+            gdaladdo_ro.sh ${in}_${lowres_str}.tif
+            #hs.sh ${in}_${lowres_str}.tif
+            #gdaladdo_ro.sh ${in}_${lowres_str}_hs_az315.tif
         fi
     fi
 }
+
+#lowres_str=${lowres}m
+lowres_str="sub9"
+
+echo "Running vrt2tif"
+echo $statlist
 export out
 export gdal_opt
 export fullres_tif
 export lowres_tif
 export lowres
+export lowres_str
 export -f stat_vrt2tif
 #Run tif generation and lowres in parallel for all stat mosaics
 statlist=$(echo $statlist | sed -e 's/lastindex/lastindex_ts/' -e 's/firstindex/firstindex_ts/' -e 's/medianindex/medianindex_ts/')
@@ -277,11 +300,11 @@ if (( "$res" == "32" )) ; then
             echo "Generating strip index shp"
             #This exports bounding boxes, but doesn't show actual footprint of valid pixels
             #gdaltindex -t_srs "$proj" ${out}_stripindex.shp $list
-            parallel -j $threads "if [ ! -e {.}_${lowres}m.tif ] ; then gdalwarp -overwrite -r cubic -t_srs \"$proj\" -tr $lowres $lowres -dstnodata -9999 {} {.}_${lowres}m.tif; fi" ::: $list
-            raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed "s/.tif/_${lowres}m.tif/g") 
+            parallel -j $threads "if [ ! -e {.}_${lowres_str}.tif ] ; then gdalwarp -overwrite -r cubic -t_srs \"$proj\" -tr $lowres $lowres -dstnodata -9999 {} {.}_${lowres_str}.tif; fi" ::: $list
+            raster2shp.py -merge_fn ${out}_stripindex.shp $(echo $list | sed "s/.tif/_${lowres_str}.tif/g") 
             #This needs more careful testing, currently deletes source files!
             #echo "Removing intermediate files"
-            #eval rm $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres}m.{shp,shx,dbf,prj,tif}/g")
+            #eval rm $(echo $list | sed "s/DEM_${res}m.tif/DEM_${res}m_${lowres_str}.{shp,shx,dbf,prj,tif}/g")
             ogr2ogr -simplify $tol ${out}_stripindex_simp.shp ${out}_stripindex.shp
             rm ${out}_stripindex.{shp,dbf,prj,shx}
             if [ ! -e ${out}_stripindex_simp.kml ] ; then 
