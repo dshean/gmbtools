2faf922339181f9d432d5f92153d75923ada8347
David Shean <dshean@gmail.com>
2018-12-09 12:57:58 -0800

    dem_post_mb_prep: update, wip

--- gmbtools/dem_post_mb_prep.sh
+++ gmbtools/dem_post_mb_prep.sh
@@ -1,37 +1,52 @@
 #! /bin/bash
 
-#After running dem_post_parallel.pbs
-#These take <1s, inefficient for sshd on large block, just check out single node and chug
-#pbs_rfe or
-#qsub -I -q devel -lselect=1:model=bro
+#After running dem_post_parallel.pbs, which generates stacks for all rgi polygons
 
-#make_mos.sh
+set -e
 
 gdal_opt='-co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER'
 
-#shp=aster_align_index_2009-2018_aea.shp
+topdir=/nobackupp8/deshean/hma/aster/dsm
+shp=dem_align_ASTER_round2_index_2000-2018_aea.shp
 
-#Generate products for consistent dates
-#parallel '~/src/gmbtools/gmbtools/stack_interp.py {}' ::: */*.npz
-#find . -name '*.npz' > npz_fn_list.txt
-#parallel --arg-file npz_fn_list.txt 'if [ ! -e {.}_20180531.tif ] ; then ~/src/gmbtools/gmbtools/stack_interp.py {}; fi'
+cd ${shp%.*}_stack
+
+#Generate products for consistent date
+echo "Generating list of npz (finished stacks)"
+if [ ! -e ${shp%.*}_npz_fn_list.txt ] ; then
+    find . -name '*.npz' > ${shp%.*}_npz_fn_list.txt
+fi
+
+echo "Running stack_interp.py for all npz"
+parallel --arg-file ${shp%.*}_npz_fn_list.txt 'if [ ! -e {.}_20180531.tif ] ; then ~/src/gmbtools/gmbtools/stack_interp.py {}; fi'
+
+#Should pass these into stack_interp.py
+mos_list="20000211 20000531 20090531 20180531 nmad trend"
+#mos_list+="trend_shpclip intercept diff diff_shpclip"
+echo "Processing the following layers:"
+echo $mos_list
 
 #difference SRTM and 20000211 grid
-#parallel --workdir $topdir/${shp%.*}_stack --sshloginfile $PBS_NODEFILE "compute_diff.py {} $rpcdem" ::: [0-9]*/*20000211.tif
+##parallel --workdir $topdir/${shp%.*}_stack --sshloginfile $PBS_NODEFILE "compute_diff.py {} $rpcdem" ::: [0-9]*/*20000211.tif
+##find . -name '*diff.tif' | parallel --workdir $topdir/${shp%.*}_stack --sshloginfile $PBS_NODEFILE 'if [ ! -e {.}_shpclip.tif ] ; then ~/src/pygeotools/pygeotools/clip_raster_by_shp.py -extent raster {} rgi ; fi'
 
-#find . -name '*diff.tif' | parallel --workdir $topdir/${shp%.*}_stack --sshloginfile $PBS_NODEFILE 'if [ ! -e {.}_shpclip.tif ] ; then ~/src/pygeotools/pygeotools/clip_raster_by_shp.py -extent raster {} rgi ; fi'
+echo "Generating fn lists for each layer"
+parallel "if [ ! -e ${shp%.*}_{}_fn_list.txt ] ; then find . -name *{}.tif > ${shp%.*}_{}_fn_list.txt ; fi" ::: $mos_list
 
-#Build vrt mosaics
-#parallel "gdalbuildvrt ${shp%.*}_stack/${shp%.*}_mos_{}.vrt ${shp%.*}_stack/[0-9]*/*{}.tif" ::: 20000211 20000531 20090531 20180531 nmad trend trend_shpclip intercept diff diff_shpclip
-#cd ${shp%.*}_stack
-#parallel "find . -name *{}.tif > {}_fn_list.txt; gdalbuildvrt -input_file_list {}_fn_list.txt ${shp%.*}_mos_{}.vrt" ::: 20000211 20000531 20090531 20180531 nmad trend 
+echo "Generating vrt for each layer"
+parallel "if [ ! -e ${shp%.*}_{}_mos.vrt ] ; then gdalbuildvrt -r cubic -input_file_list ${shp%.*}_{}_fn_list.txt ${shp%.*}_{}_mos.vrt; fi" ::: $mos_list
 
-#Export tif
-#Regenerate tiles, much more efficient than large vrt
-#parallel "mkdir {.}; gdal_retile.py -r cubic $gdal_opt -ps 2048 2048 -targetDir {.} {}" ::: *vrt
-#ndv=-9999
-#parallel "gdalbuildvrt -srcnodata $ndv -vrtnodata $ndv {.}_tiled.vrt ${shp%.*}_mos_{}/*.tif" :::  20000211  20000531 20090531 20180531 nmad trend
+#Regenerate tiles, much more efficient than working with vrt containing 90K files
+echo "Retiling vrts"
+parallel "mkdir ${shp%.*}_{}_mos_retile; gdal_retile.py -r cubic $gdal_opt -ps 2048 2048 -targetDir ${shp%.*}_{}_mos_retile ${shp%.*}_{}_mos.vrt; " ::: $mos_list
+
+echo "Generating new vrt for retile"
 #Set nodata
-#parallel --workdir $topdir/${shp%.*}_stack --sshloginfile $PBS_NODEFILE "gdalwarp -r cubic $gdal_opt ${shp%.*}_mos_{}.vrt ${shp%.*}_mos_trend.tif; gdaladdo_ro.sh ${shp%.*}_mos_{}.tif" ::: trend trend_shpclip diff diff_shpclip
+ndv=-9999
+#Shouldn't need -r cubic here, as these are regular grid
+parallel "gdalbuildvrt -srcnodata $ndv -vrtnodata $ndv ${shp%.*}_{}_mos_retile.vrt ${shp%.*}_{}_mos_retile/*.tif" ::: $mos_list 
+
+echo "Converting to tif"
+parallel "gdalwarp $gdal_opt ${shp%.*}_{}_mos_retile.vrt ${shp%.*}_{}_mos_retile.tif; gdaladdo_ro.sh ${shp%.*}_{}_mos_retile.tif" ::: trend nmad 
 
 #Now mb_parallel.py
