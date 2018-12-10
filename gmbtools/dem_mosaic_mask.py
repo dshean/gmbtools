b75cdaceb3cea2667cc85af63cb0d6b72f6712c2
David Shean <dshean@gmail.com>
2018-12-09 19:30:41 -0800

    dem_mosaic_mask: cascading masking, write out regardless of pixel count

--- gmbtools/dem_mosaic_mask.py
+++ gmbtools/dem_mosaic_mask.py
@@ -22,34 +22,38 @@ def domask(tile_fn):
     mask = np.ma.getmaskarray(dem)
     valid_px_count = (~mask).sum()
     if valid_px_count < 1:
-        sys.exit("No valid pixels remain")
-    print("Valid pixel count: %i" % valid_px_count)
+        print("No valid pixels remain")
+    else:
+        print("Valid pixel count: %i" % valid_px_count)
+        min_count = 2
+        count_fn = prefix+'-count.tif'
+        print("Loading: %s" % count_fn)
+        count = iolib.fn_getma(count_fn)
+        print("min: %i max: %i" % (count.min(), count.max()))
+        print("Masking: (count < %i)" % min_count)
+        mask = np.logical_or(mask, (count < min_count))
+        valid_px_count = (~mask).sum()
+        if valid_px_count < 1:
+            print("No valid pixels remain")
+        else:
+            print("Valid pixel count: %i" % valid_px_count)
 
-    min_count = 2
-    count_fn = prefix+'-count.tif'
-    print("Loading: %s" % count_fn)
-    count = iolib.fn_getma(count_fn)
-    print("min: %i max: %i" % (count.min(), count.max()))
-    print("Masking: (count < %i)" % min_count)
-    mask = np.logical_or(mask, (count < min_count))
-    valid_px_count = (~mask).sum()
-    if valid_px_count < 1:
-        sys.exit("No valid pixels remain")
-    print("Valid pixel count: %i" % valid_px_count)
+            max_std = 3.0
+            #std_fn = prefix+'-std.tif'
+            std_fn = prefix+'-nmad.tif'
+            print("Loading: %s" % std_fn)
+            std = iolib.fn_getma(std_fn)
+            print("min: %i max: %i" % (std.min(), std.max()))
+            print("Masking: (std/nmad >= %i)" % max_std)
+            mask = np.logical_or(mask, (std >= max_std))
+            valid_px_count = (~mask).sum()
+            if valid_px_count < 1:
+                print("No valid pixels remain")
+            else:
+                print("Valid pixel count: %i" % valid_px_count)
 
-    max_std = 3.0
-    #std_fn = prefix+'-std.tif'
-    std_fn = prefix+'-nmad.tif'
-    print("Loading: %s" % std_fn)
-    std = iolib.fn_getma(std_fn)
-    print("min: %i max: %i" % (std.min(), std.max()))
-    print("Masking: (std/nmad >= %i)" % max_std)
-    mask = np.logical_or(mask, (std >= max_std))
-    valid_px_count = (~mask).sum()
-    if valid_px_count < 1:
-        sys.exit("No valid pixels remain")
-    print("Valid pixel count: %i" % valid_px_count)
-
+    #Modified so we always write out, even if empty tif
+    #Easier for tracking progress
     print("Applying mask")
     dem_masked = np.ma.array(dem, mask=mask)
     out_fn = os.path.splitext(dem_fn)[0]+'_masked.tif'
